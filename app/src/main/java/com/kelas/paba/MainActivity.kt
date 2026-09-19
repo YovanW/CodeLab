79b04c0bb8d88c3234cb9dfeaecdc18f3e5d706e
YovanW <yovanwisely@gmail.com>
2026-09-19 17:25:33 +0700

    Last Push

--- app/src/main/java/com/kelas/paba/MainActivity.kt
+++ app/src/main/java/com/kelas/paba/MainActivity.kt
@@ -14,19 +14,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.kelas.paba.ui.theme.PABATheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PABATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
